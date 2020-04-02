107436e2a015669f3ad7fbc40288dcbe5ffeba32
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-04-02 18:01:36 +0800

    added a navigator bar that displays placeholder widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,74 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(SettingsPage());
+void main() => runApp(BottomNavigatorView());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+List<Widget> screens = [Text('Chat'), Text('Settings')];
+
+// class ChatPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     // Todo: use state to manage bubble nips, color, and name
+//     Widget chatBubble = Bubble(
+//       child: Text('Hello World'),
+//     );
+
+//     return MaterialApp(
+//       title: 'TeryGram',
+//       theme: ThemeData(
+//         primarySwatch: Colors.purple,
+//       ),
+//       home: Scaffold(
+//           bottomNavigationBar: BottomNavigationBarWidget(),
+//           appBar: AppBar(centerTitle: true, title: Text('TeryGram')),
+//           body: ListView(
+//             children: <Widget>[chatBubble],
+//           )
+//           // in these two columns contain the text bubble and the user icon
+//           ),
+//     );
+//   }
+// }
+
+// Settings Page model
+class BottomNavigatorView extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TeryGram',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('TeryGram')),
-        body: Column(children: [
-          SettingsPage(),
-        ]),
-      ),
-    );
-  }
+  _BottomNavigatorViewState createState() => _BottomNavigatorViewState();
 }
 
-class SettingsPage extends StatelessWidget {
+class _BottomNavigatorViewState extends State<BottomNavigatorView> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Settings',
       theme: ThemeData(primarySwatch: Colors.purple),
       home: Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Container(
-              // tried to contain text in a container to add padding so as to make the appbar bigger, but did not work
-              child: Text('Settings'),
-              margin: EdgeInsets.all(50),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              title: Text('Chat'),
             ),
-          ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), title: Text('Settings')),
+          ],
+          currentIndex: currentIndex,
+          onTap: (int newIndex) {
+            /// When an icon in the [BottomNavigationBar] is tapped,
+            /// we want to change the current view to the new view
+            /// representented by the recently tapped
+            /// [BottomNavigationBarItem].
+            setState(() {
+              currentIndex = newIndex;
+            });
+          },
         ),
+        appBar: AppBar(
+          centerTitle: true,
+          // Todo: Make appbar bigger
+          title: Text('Settings'),
+        ),
+        body: screens[currentIndex],
       ),
     );
   }
