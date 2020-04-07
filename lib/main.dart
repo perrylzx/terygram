3f2726f0e1d4536b9ca3e97abc2d35f00b184e27
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-04-07 15:36:10 +0800

    moved bottomnavigatorview to seperate folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:terygram/BottomNavigatorView.dart';
 
-void main() => runApp(BottomNavigatorView());
+void main() => runApp(Home());
 
-List<Widget> screens = [Text('Chat'), Text('Settings')];
-
-// Settings Page model
-class BottomNavigatorView extends StatefulWidget {
+class Home extends StatefulWidget {
   @override
-  _BottomNavigatorViewState createState() => _BottomNavigatorViewState();
+  _HomeState createState() => _HomeState();
 }
 
-class _BottomNavigatorViewState extends State<BottomNavigatorView> {
-  int currentIndex = 0;
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Settings',
-      theme: ThemeData(primarySwatch: Colors.purple),
       home: Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.chat),
-              title: Text('Chat'),
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.settings), title: Text('Settings')),
-          ],
-          currentIndex: currentIndex,
-          onTap: (int newIndex) {
-            /// When an icon in the [BottomNavigationBar] is tapped,
-            /// we want to change the current view to the new view
-            /// representented by the recently tapped
-            /// [BottomNavigationBarItem].
-            setState(() {
-              currentIndex = newIndex;
-            });
-          },
-        ),
-        appBar: AppBar(
-          centerTitle: true,
-          // Todo: Make appbar bigger
-          title: Text('Settings'),
-        ),
-        body: screens[currentIndex],
+        body: Container(child: BottomNavigatorView()),
       ),
     );
   }
