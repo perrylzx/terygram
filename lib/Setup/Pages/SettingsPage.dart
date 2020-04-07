9b45ca0910f8e7cb380f91f391bfed3a4de8649c
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-04-07 18:01:01 +0800

    change text to say settings

--- lib/Setup/Pages/SettingsPage.dart
+++ lib/Setup/Pages/SettingsPage.dart
@@ -9,12 +9,7 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget textWidget = Container(
     padding: const EdgeInsets.all(32), // adding 32px padding to all sides
     child: Text(
-      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-      'Alps. Situated 1,578 meters above sea level, it is one of the '
-      'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-      'half-hour walk through pastures and pine forest, leads you to the '
-      'lake, which warms to 20 degrees Celsius in the summer. Activities '
-      'enjoyed here include rowing, and riding the summer toboggan run.',
+      'Log Out',
       softWrap: true,
     ),
   );
